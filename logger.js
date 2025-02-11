45ddcdd76cb9374e87a99b74c65799cf7d07a683
800147 <800147@gmail.com>
2025-02-11 17:04:10 +0300

    group methods support

--- logger.js
+++ logger.js
@@ -43,17 +43,58 @@
 :where(.OSLogger-Number) {
   color: darkgreen;
 }
+
+:where(.OSLogger-Group > :first-child) {
+  margin-left: 0.5em;
+}
+
+:where(.OSLogger-Group) {
+  position: relative;
+  margin-left: 0;
+}
+
+:where(.OSLogger-Group)::before, :where(.OSLogger-Group)::after {
+  content: '';
+  display: block;
+  width: 0.125em;
+  position: absolute;
+  z-index: -1;
+  inset: 0.5em auto 0.25em 0.625em;
+  background-color: black;
+}
+
+:where(.OSLogger-Group)::after {
+  inset: 0.5em auto 0.25em 0.5em;
+  background-color: white;
+}
+
+:where(.OSLogger-Group > :not(:first-child)) {
+  margin-left: 1.5em;
+}
+
+:where(.OSLogger-Group > .OSLogger-Group:not(:first-child)) {
+  margin-left: 1em;
+}
+
 `;
 
   const loggerRoot = Object.assign(document.createElement("div"), {
     className: "OSLogger",
     inert: true,
   });
+  const oldConsole = { ...console };
+  const counts = Object.create(null);
+  const timers = Object.create(null);
+  let slot = loggerRoot;
 
   const scrollToBottom = () => loggerRoot.scroll(0, loggerRoot.scrollHeight);
   const round1000 = (num) => Math.round(num * 1000) / 1000;
-
-  const oldConsole = { ...console };
+  const unsupported =
+    (funcName) =>
+    (...args) => {
+      print(args, { unsupported: funcName });
+      oldConsole[funcName]?.apply(console, args);
+    };
 
   const print = (args, { unsupported, type = "log" } = {}) => {
     if (unsupported) {
@@ -123,53 +164,70 @@
       content.appendChild(document.createTextNode(")"));
     }
 
-    loggerRoot.appendChild(p);
+    slot.appendChild(p);
     scrollToBottom();
   };
 
-  const unsupported =
-    (funcName) =>
-    (...args) => {
-      print(args, { unsupported: funcName });
-      oldConsole[funcName].apply(console, args);
-    };
+  const timeLog = (label = "dafault", isEnd) => {
+    if (timers[label] === undefined) {
+      print([`Timer "${label}" doesn't exist`], { type: "warn" });
+    } else {
+      print([
+        `${label}: ${round1000(performance.now() - timers[label])}ms${
+          isEnd ? " — ended" : ""
+        }`,
+      ]);
+      if (isEnd) {
+        delete timers[label];
+      }
+    }
+  };
 
-  const counts = Object.create(null);
-  const timers = Object.create(null);
+  const group = (label, isCollapse) => {
+    const groupEl = Object.assign(document.createElement("div"), {
+      className: "OSLogger-Group",
+    });
+    const groupName = label ? ` "${label}"` : "";
+    const collapseMessage = isCollapse ? " (couldn't collapse)" : "";
+    slot.appendChild(groupEl);
+    slot = groupEl;
+    print([`Group${groupName}${collapseMessage}`]);
+  };
 
   Object.assign(console, {
     log: (...args) => {
       print(args, { type: "log" });
-      oldConsole.log.apply(console, args);
+      oldConsole.log?.apply(console, args);
     },
     error: (...args) => {
       print(args, { type: "error" });
-      oldConsole.error.apply(console, args);
+      oldConsole.error?.apply(console, args);
     },
     info: (...args) => {
       print(args, { type: "info" });
-      oldConsole.info.apply(console, args);
+      oldConsole.info?.apply(console, args);
     },
     warn: (...args) => {
       print(args, { type: "warn" });
-      oldConsole.warn.apply(console, args);
+      oldConsole.warn?.apply(console, args);
     },
     clear: (...args) => {
       loggerRoot.textContent = "";
-      oldConsole.clear.apply(console, args);
+      slot = loggerRoot;
+      oldConsole.clear?.apply(console, args);
     },
     assert: (...args) => {
       const [flag, ...rest] = args;
       if (!flag) {
         print(["Assertion failed:", ...rest], { type: "error" });
       }
-      oldConsole.assert.apply(console, args);
+      oldConsole.assert?.apply(console, args);
     },
     count: (...args) => {
       const label = args[0] ?? "dafault";
       counts[label] = (counts[label] ?? 0) + 1;
       print([`${label}:`, counts[label]]);
-      oldConsole.count.apply(console, args);
+      oldConsole.count?.apply(console, args);
     },
     countReset: (...args) => {
       const label = args[0] ?? "dafault";
@@ -179,7 +237,7 @@
         counts[label] = 0;
         print([`${label}:`, counts[label]]);
       }
-      oldConsole.countReset.apply(console, args);
+      oldConsole.countReset?.apply(console, args);
     },
     time: (...args) => {
       const label = args[0] ?? "dafault";
@@ -188,36 +246,34 @@
       } else {
         timers[label] = performance.now();
       }
-      oldConsole.time.apply(console, args);
+      oldConsole.time?.apply(console, args);
     },
     timeLog: (...args) => {
-      const label = args[0] ?? "dafault";
-      if (timers[label] === undefined) {
-        print([`Timer "${label}" doesn't exist`], { type: "warn" });
-      } else {
-        print([`${label}: ${round1000(performance.now() - timers[label])}ms`]);
-      }
-      oldConsole.timeLog.apply(console, args);
+      timeLog(args[0]);
+      oldConsole.timeLog?.apply(console, args);
     },
     timeEnd: (...args) => {
-      const label = args[0] ?? "dafault";
-      if (timers[label] === undefined) {
-        print([`Timer "${label}" doesn't exist`], { type: "warn" });
-      } else {
-        print([
-          `${label}: ${round1000(performance.now() - timers[label])}ms — ended`,
-        ]);
-        delete timers[label];
+      timeLog(args[0], true);
+      oldConsole.timeEnd?.apply(console, args);
+    },
+    group: (...args) => {
+      group(args[0]);
+      oldConsole.group?.apply(console, args);
+    },
+    groupCollapsed: (...args) => {
+      group(args[0], true);
+      oldConsole.groupCollapsed?.apply(console, args);
+    },
+    groupEnd: (...args) => {
+      if (slot !== loggerRoot) {
+        slot = slot.parentElement;
       }
-      oldConsole.timeEnd.apply(console, args);
+      oldConsole.groupEnd?.apply(console, args);
     },
 
     debug: unsupported("debug"),
     dir: unsupported("dir"),
     dirxml: unsupported("dirxml"),
-    group: unsupported("group"),
-    groupCollapsed: unsupported("groupCollapsed"),
-    groupEnd: unsupported("groupEnd"),
     table: unsupported("table"),
     timeStamp: unsupported("timeStamp"),
     trace: unsupported("trace"),
